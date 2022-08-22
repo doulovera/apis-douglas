46c87bfb59e2e2fd675a14ab33d3bdd90984b038
Douglas Lovera <doulovera@gmail.com>
2022-08-21 20:47:16 -0500

    fix: handle response

--- pages/api/webhook/games/minecraft-reddit.ts
+++ pages/api/webhook/games/minecraft-reddit.ts
@@ -23,18 +23,23 @@ export default async function handler(
   if (!subreddit.includes('minecraft')) return res.status(200).json({ message: 'ok' });
 
   try {
-    await fetch(`https://discord.com/api/webhooks/1011069949827100712/${DISCORD_MINECRAFT_TOKEN}`, {
+    const response = await fetch(`https://discord.com/api/webhooks/1011069949827100712/${DISCORD_MINECRAFT_TOKEN}`, {
       method: 'POST',
       "headers": {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
       },
       body: JSON.stringify({
         content: `New saved post: ${url}`,
       }),
     });
+    if (response.ok) {
+      res.status(200).json({ message: 'Sent to webhook' });
+    } else {
+      res.status(500).json({ error: 'Error sending to webhook' });
+    }
   } catch (error) {
     res.status(400).json({ error: (error as Error).message });
   }
 
-  res.status(200).json({ message: 'Sent to webhook' });
 }
